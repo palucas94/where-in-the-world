eb69d88c96ffbd7945e41b7eb0c0e4ebebfd046c
Lucas Pontes Arruda <paluke94@gmail.com>
2022-04-16 09:07:31 -0300

    [feat] add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header-container">
+      <h2 className="header-title">Where in the world?</h2>
+    </div>
+  );
+}
+
+export default Header;
